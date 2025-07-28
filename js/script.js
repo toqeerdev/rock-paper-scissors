9031e5a1fadec2f0c6f85f647bc03ad33a7c40dd
Toqeer <187836534+Toqeer124@users.noreply.github.com>
2025-07-28 14:54:44 +0500

    Write the getComputerChoice function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log("Hello, World!");
+function getComputerChoice() {
+ let max = 3
+ let computerChoice = Math.floor(Math.random() * max);
+
+ if (computerChoice === 0) {
+    return "rock";
+ }
+ else if (computerChoice === 1) {
+    return "paper";
+ }
+ else {
+    return "scissors";
+ }
+   
+}
+
+
+const computerSelection = getComputerChoice();
+
