e46d917974029433c0cc52008ace3f7db4d35ee8
Toqeer <187836534+Toqeer124@users.noreply.github.com>
2025-07-29 15:05:22 +0500

    Write the playRound function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,37 @@
-function getComputerChoice() {
- let max = 3
+function playRound(humanChoice, computerChoice) {
+   humanChoice = humanChoice.toLowerCase();
+
+   if (humanChoice === "rock" && computerChoice === "paper") {
+      console.log("You lose! Paper beats rock.");
+      computerScore += 5;
+   }
+   else if (humanChoice === "rock" && computerChoice === "scissors") {
+      console.log("You win! Rock beats scissors.");
+      humanScore += 5;
+   }
+   else if (humanChoice === "paper" && computerChoice === "rock") {
+      console.log("You win! Paper beats rock.");
+      humanScore += 5;
+   }
+   else if (humanChoice === "paper" && computerChoice === "scissors") {
+      console.log("You lose! Scissors beat paper.");
+      computerScore += 5;
+   }
+   else if (humanChoice === "scissors" && computerChoice === "rock") {
+      console.log("You lose! Rock beats scissors.");
+      computerScore += 5;
+   }
+   else if (humanChoice === "scissors" && computerChoice === "paper") {
+      console.log("You win! Scissors beat paper.");
+      humanScore += 5;
+   }
+   else if (humanChoice === computerChoice) {
+      console.log("It's a draw.");
+   }
+}
+
+
+function getComputerChoice(max) {
  let computerChoice = Math.floor(Math.random() * max);
 
  if (computerChoice === 0) {
@@ -16,14 +48,14 @@ function getComputerChoice() {
 
 function getHumanChoice()
 {
-    let humanChoice = prompt("Enter your choice: ", "rock");
+    let humanChoice = prompt("Enter your choice: ", "Rock");
     return humanChoice;
 }
 
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-
 let humanScore = 0;
 let computerScore = 0;
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice(3);
+
+playRound(humanSelection, computerSelection);
