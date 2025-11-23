1ec2a18c635e34223dfc274860fc4c779f3f792a
Toqeer <187836534+Toqeer124@users.noreply.github.com>
2025-11-23 21:16:53 +0500

    Write the playRound() function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-    const computerChoice = Math.floor(Math.random() * 3)
+    let computerChoice = Math.floor(Math.random() * 3)
     if (computerChoice === 0) {
         return "rock";
     }
@@ -15,9 +15,45 @@ function getHumanChoice() {
     return prompt("Enter your choice: ");
 }
 
+function playRound(caseInsensitiveHumanChoice, computerChoice) {
+    let humanChoice = caseInsensitiveHumanChoice.toLowerCase();
+   
+    if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log("You win! Rock beats Scissors!");
+        humanScore += 5;
+    }
+    else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log("You win! Paper beats Rock!");
+        humanScore += 5;
+    }
+    else if (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log("You win! Scissors beat Paper!");
+        humanScore += 5;
+    }
+    else if (humanChoice === "rock" && computerChoice === "paper") {
+        console.log("You lose! Paper beats Rock!");
+        computerScore += 5;
+    }
+    else if (humanChoice === "paper" && computerChoice === "scissors") {
+        console.log("You lose! Scissors beat Paper!");
+        computerScore += 5;
+    }
+    else if (humanChoice === "scissors" && computerChoice === "rock") {
+        console.log("You lose! Rock beats Scissors")
+        computerScore += 5;
+    }
+    else {
+        console.log("It's a draw.")
+    }
+}
 
-const computerChoice = getComputerChoice();
-const humanChoice = getHumanChoice();
+const computerSelection = getComputerChoice();
+const humanSelection = getHumanChoice();
 
 let humanScore = 0;
 let computerScore = 0;
+
+playRound(humanSelection, computerSelection);
+
+console.log(humanScore, computerScore);
+
